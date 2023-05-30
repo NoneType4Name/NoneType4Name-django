39d8b56ef9f6dd539db53b702d6c44afe27a5c54
AlexKim0710 <90710372+NoneType4Name@users.noreply.github.com>
2023-05-30 19:13:17 +0300

    deploy.

--- NoneType4Name/settings.py
+++ NoneType4Name/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-clc_--7yn7m&7-x2-g%*rn9z4^rev&b!-gr0$*f6^9sqgt99em'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.alwaysdata.net', '127.0.0.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
